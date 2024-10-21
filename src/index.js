c51f5fab93dc69cf4c86c2cb463ac9ef918f7e57
Ioseb Khetsuriani <94854102+iosebkhe@users.noreply.github.com>
2024-10-21 05:23:33 +0400

    NEW feature: implement tab switching functionality and structure reorganization
    - Moved tab-related code to `tabs.js` for better modularity and maintainability.
    - Exported the tab functionality from `tabs.js` and imported it into `index.js`.
    - Created a `components` folder and added a `tabs.scss` file for tab-specific styles.
    - Updated tab click behavior to prevent form submission when switching tabs.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/main.scss';
 import { initInputValidation, initSearchButton } from './js/form-validation';
+import { initTabs } from './js/tabs';
 
 (function init() {
   initInputValidation();
   initSearchButton();
+  initTabs();
 })();
